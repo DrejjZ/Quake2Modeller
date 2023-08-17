ee6f35441fb64569d10ddcbd77d27fb9cb1194de
DrejjZ <timmothyz@gmail.com>
2023-08-17 00:14:44 -0300

    First Commit

---
+++ DlgGlobs.h
@@ -0,0 +1,17 @@
+//---------------------------------------------------------------------------
+#ifndef DialogGlobalsH
+#define DialogGlobalsH
+
+#include "A4MDLDlg.h"
+#include "SkinDlg.h"
+#include "PaintDlg.h"
+#include "TypeDlg.h"
+
+extern CA4MDLDlg        *g_MainDlg;
+extern CSkinEditorDlg   *g_SkinEditDlg;
+extern CPaintDlg        *g_PaintDlg;
+extern CTypeInDlg       *g_TypeInDlg;
+
+
+
+#endif   // globals H
